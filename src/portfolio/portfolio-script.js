a3dac0ef464134569a47d172f795ebd6e72cde57
jeanitha <147796844+jeanitha@users.noreply.github.com>
2024-02-24 13:41:23 +0800

    arrow

--- src/portfolio/portfolio-script.js
+++ src/portfolio/portfolio-script.js
@@ -1,24 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const card1 = document.querySelector('.project-item .card-1');
-    const card2 = document.querySelector('.project-item .card-2');
-    const card3 = document.querySelector('.project-item .card-3');
-  
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5,
-    };
-  
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('fade-in');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, options);
-  
-    observer.observe(card1);
-    observer.observe(card2);
-    observer.observe(card3);
+document.addEventListener("DOMContentLoaded", function () {
+  const card1 = document.querySelector('.project-item .card-1');
+  const card2 = document.querySelector('.project-item .card-2');
+  const card3 = document.querySelector('.project-item .card-3');
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  observer.observe(card1);
+  observer.observe(card2);
+  observer.observe(card3);
+});
+
+// scroll to top of page on refresh
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+// for landing page arrow click
+$(document).ready(function () {
+  $('#down').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $('#start').offset().top
+    }, 500);
   });
+});
