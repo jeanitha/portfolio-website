49676a19ca4bc66d81d3e5099c8f3f7e5b3b88a8
jeanitha <147796844+jeanitha@users.noreply.github.com>
2024-02-27 17:37:45 +0800

    added projects

--- src/portfolio/portfolio-script.js
+++ src/portfolio/portfolio-script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const card1 = document.querySelector('.project-item .card-1');
   const card2 = document.querySelector('.project-item .card-2');
   const card3 = document.querySelector('.project-item .card-3');
+  const card4 = document.querySelector('.project-item .card-4');
+  const card5 = document.querySelector('.project-item .card-5');
 
   const options = {
     root: null,
@@ -21,6 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
   observer.observe(card1);
   observer.observe(card2);
   observer.observe(card3);
+  observer.observe(card4);
+  observer.observe(card5);
 });
 
 // scroll to top of page on refresh
