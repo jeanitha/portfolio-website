1834c789ae4a43fe7074b0866209acefee606fe4
jeanitha <147796844+jeanitha@users.noreply.github.com>
2024-03-13 02:06:30 +0800

    latest

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 30) {
+    if ($(this).scrollTop() > 50) {
       $('.headshot').css({
         'transform': 'translateY(-100px)',
         'transition': 'transform 750ms ease-in-out'
