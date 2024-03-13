9c9987983db89ea042e6fa729c1c60dae8f97605
jeanitha <147796844+jeanitha@users.noreply.github.com>
2024-03-13 14:34:34 +0800

    animated

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,32 @@ $(document).ready(function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const card1 = document.querySelector('.timeline .event-1');
+  const card2 = document.querySelector('.timeline .event-2');
+  const card3 = document.querySelector('.timeline .event-3');
+  const card4 = document.querySelector('.timeline .event-4');
+  const card5 = document.querySelector('.timeline .event-5');
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  observer.observe(card1);
+  observer.observe(card2);
+  observer.observe(card3);
+  observer.observe(card4);
+  observer.observe(card5);
+});
